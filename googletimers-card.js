4164772825a011e0bae70cd416181f456c3d72f6
Simon Hansen <67142049+DurgNomis-drol@users.noreply.github.com>
2021-03-07 17:17:20 +0100

    Rewrote card and add ability to show fire time

--- googletimers-card.js
+++ googletimers-card.js
@@ -66,9 +66,11 @@ class GoogleTimersCard extends HTMLElement {
           margin: 8px 4px -5px;
         }
         .title {
-          font-size: 0.9em;
+          color: var(--secondary-text-color);
+          font-size: 1.2em;
           padding: 0 5px 0 5px;
           text-transform: capitalize;
+          font-weight: 500;
         }
         .duration {
           font-size: 0.7em;
@@ -80,14 +82,43 @@ class GoogleTimersCard extends HTMLElement {
       this.appendChild(card);
     }
 
+    const STATE_ON = "on";
+    const STATE_UNKOWN = "unknown"
+
+    const NO_TIMERS = "No timers set.";
+    const TIMER_IS_DONE = "TIMER DONE!";
+
+    const JSON_TIMERS = "timers"
+    const JSON_DURATION = "duration";
+    const JSON_LOCAL_TIME = "local_time";
+    const JSON_NAME = "label";
+
+    const ICON_ALARM = "mdi:alarm";
+    const ICON_ALARM_DONE = "mdi:bell-ring";
+    const ICON_DURATION = "mdi:timelapse";
+    const ICON_ALARM_TIME = "mdi:clock";
+    const ICON_LABEL = "mdi:label-variant"
+
+    const STRING_HOURS = " h. "
+    const STRING_MINUTES = " mins. "
+    const STRING_SECONDS = " secs."
+
+    function get_timedelta(time) {
+        return new Date(Date.parse(time) - Date.now());
+    }
+
+    function format_to_human_readable(remaining_time) {
+        var hours = remaining_time.getHours() > 1 ? remaining_time.getHours() - 1 + STRING_HOURS : ""
+        var minutes = remaining_time.getMinutes() < 10  && remaining_time.getHours() > 1 ? "0"+ remaining_time.getMinutes() : remaining_time.getMinutes();
+        var seconds = remaining_time.getSeconds() < 10 ? "0"+ remaining_time.getSeconds() : remaining_time.getSeconds();
+        var time_to_show = hours + minutes + STRING_MINUTES + seconds + STRING_SECONDS;
+        return time_to_show;
+    }
+
     var timers = [];
 
-    if (state.state != 'unknown') {
-      var device = {
-        'state': state.state,
-      };
-
-      timers = [device].concat(state.attributes['timers']);
+    if (state.state != STATE_UNKOWN) {
+      timers = state.attributes[JSON_TIMERS];
     }
 
     var html = `
@@ -97,43 +128,47 @@ class GoogleTimersCard extends HTMLElement {
     </div>
     `;
 
-    if (device['state'] == "on") {
+    if (state.state == STATE_ON) {
       for (const timer of timers) {
-        if (timer['duration'] === undefined) {
+        if (timer[JSON_DURATION] === undefined) {
           continue;
         }
 
-        var timer_name = timer['label'] ? "<ha-icon style='padding: 0 3px 0 0; --mdc-icon-size: 1em;' icon='mdi:label'></ha-icon>" + timer['label'] : "";
+        var timer_name = ""
+        var alarm_time = ""
+        var icon = ICON_ALARM
 
-        var remaining_time = new Date(Date.parse(timer['local_time']) - Date.now());
-        var hours = remaining_time.getHours() > 1 ? remaining_time.getHours() - 1 + " h. " : ""
-        var minutes = remaining_time.getMinutes() < 10  && remaining_time.getHours() > 1 ? "0"+ remaining_time.getMinutes() : remaining_time.getMinutes();
-        var seconds = remaining_time.getSeconds() < 10 ? "0"+ remaining_time.getSeconds() : remaining_time.getSeconds();
-        var time_to_show = hours + minutes +" mins. "+ seconds + " secs.";
+        var remaining_time = get_timedelta(timer[JSON_LOCAL_TIME])
+        var formatted_time = format_to_human_readable(remaining_time)
 
         if (Math.sign(remaining_time) == -1) {
-          html += `
-          <div class="info" style="margin: -5px 0 -5px;">
-            <div class="icon"><ha-icon style="padding: 0 5px 0 0; color: orange; --mdc-icon-size: 24px;" icon="mdi:bell-ring"></ha-icon></div>
-            <div class="timer">TIMER DONE!<span class="title">${timer_name}</span></div>
-          </div>
-          `;
-          continue;
+          formatted_time = TIMER_IS_DONE
+          icon = ICON_ALARM_DONE
+        }
+
+        if (this.config.show_fire_time) {
+          alarm_time = "<span class='duration'><ha-icon style='padding: 0 3px 0 0; --mdc-icon-size: 1.1em;' icon='" + ICON_ALARM_TIME + "'></ha-icon>" + timer[JSON_LOCAL_TIME].split(" ")[1] + "</span>"
+        }
+
+        if (timer[JSON_NAME]) {
+          timer_name = "<div style='margin: 0 15px 0 15px;'><span class='title'><ha-icon style='padding: 0 3px 0 0; --mdc-icon-size: 1.1em;' icon='" + ICON_LABEL + "'></ha-icon>" + timer[JSON_NAME] + "</span></div>"
         }
 
         html += `
-        <div class="info" style="margin: -5px 0 -5px;">
-          <div class="icon"><ha-icon style="padding: 0 5px 0 0; --mdc-icon-size: 24px;" icon="mdi:alarm"></ha-icon></div>
-          <div class="timer">${time_to_show}<span class="title">${timer_name}</span><span class="duration"><ha-icon style="padding: 0 3px 0 0; --mdc-icon-size: 1em;" icon="mdi:timelapse"></ha-icon>${timer['duration']}</span></div>
+        <div>
+          ${timer_name}
+          <div class="info" style="margin: -5px 0 -5px;">
+            <div class="icon"><ha-icon style="padding: 0 5px 0 0; --mdc-icon-size: 24px;" icon="${icon}"></ha-icon></div>
+            <div class="timer">${formatted_time}<span class="duration"><ha-icon style="padding: 0 3px 0 0; --mdc-icon-size: 1.1em;" icon="${ICON_DURATION}"></ha-icon>${timer[JSON_DURATION]}</span>${alarm_time}</div>
+          </div>
         </div>
         `;
       }
 
     } else {
-      var is_off = "No timers set."
       html += `
       <div class="info">
-        <span class="value">${is_off}</span>
+        <span class="value">${NO_TIMERS}</span>
       </div>
       `;
     }
